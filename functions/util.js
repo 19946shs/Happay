904fe18ce0a226e0760c72a88428c1d70931f4af
unknown <shsurya20@gmail.com>
2022-06-11 18:23:24 +0530

    minor changes to spinner

--- functions/util.js
+++ functions/util.js
@@ -21,26 +21,31 @@ const showDecisionPrompt = (prompt) => {
     });
 }
 
-const showSpinnerOrNot = (show) => {
+const showSpinnerOrNot = (showornot, interval_ = 0) => {
   process.stdout.write('\x1B[?25l')
   const spinners = ['-', '\\', '|', '/']
   let index = 0
 
-  const interval = setInterval(() => {
-    let line = spinners[index]
-    if (line === undefined) {
-      index = 0
-      line = spinners[index]
-    }
-
-    std.write(line)
-    rdl.cursorTo(std, 0, 0)
-
-    index = index >= spinners.length ? 0 : index + 1
-  }, 100)
-
-  if(!show) clearInterval(interval)
+  if (showornot) {
+    process.stdout.write('\033c')
+    const interval = setInterval(() => {
+      let line = spinners[index]
+      if (line === undefined) {
+        index = 0
+        line = spinners[index]
+      }
+  
+      std.write(line)
+      rdl.cursorTo(std, 0, 0)
+  
+      index = index >= spinners.length ? 0 : index + 1
+    }, 100)
+    return interval
+  } else {
+    interval_.clearInterval()
+  }
+  
 }
 
 module.exports = showDecisionPrompt
-// module.exports = showSpinnerOrNot
+module.exports = showSpinnerOrNot
